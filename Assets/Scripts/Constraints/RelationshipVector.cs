9d77a7a8bbd8c3955da1822fcd691c92c366e0b9
Sarah Noonan <sanoonan@tcd.ie>
2015-08-07 10:12:25 +0100

    Modified relationship vector

--- Assets/Scripts/Constraints/RelationshipVector.cs
+++ Assets/Scripts/Constraints/RelationshipVector.cs
@@ -1,18 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public enum RelationshipValue
 {
-    PosNeg,
-    Closeness
+    Friendship,      //platonic
+    Attraction,    //romantic
 };
 
 
 public class RelationshipVector
 { 
-    private int numValues;
-    private float[] values;
+    private const float _minValue = 0.0f;
+    private const float _maxValue = 10.0f;
+    private const float _startingFamiliarity = 1.0f;
+    private const float _neutralValue = 5.0f;
+
+    private int _numValues;
+    private float[] _values;
+    private float _familiarity;
 
     public RelationshipVector()
     {
@@ -21,8 +27,32 @@ public class RelationshipVector
 
         values = new float[numValues];
 
-
         for (int i = 0; i < numValues; i++)
-            values[i] = 0.0f;
+            values[i] = _neutralValue;
+            
+        _familiarity = _startingFamiliarity;
     }
+
+    public void MakeRandomExistingRelationship()
+    {
+        RandomiseFamiliarity();
+        RandomiseValues();
+    }
+    public void MakeRandomNewRelationship()
+    {
+        RandomiseValues();
+    }
+    
+    private void RandomiseValues()
+    {
+        for( int i=0; i<_numValues; i++ )
+        {
+            _values[i] = Random.Range( _minValue, _maxValue );
+        }
+    }
+    private void RandomiseFamiliarity()
+    {
+        _familiarity = Random.Range( _minValue, _maxValue );
+    }
+
 }
